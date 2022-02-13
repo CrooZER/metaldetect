ebf88edcc4bf2c7be0824981daa6c717f762338e
cheban-artem <artem.cheban@dataart.com>
2022-02-13 20:36:12 +0200

    add new models

--- metaldetect/models.py
+++ metaldetect/models.py
@@ -1,3 +1,7 @@
+from datetime import date
+from operator import mod
+from os import name
+from pyexpat import model
 from django.db import models
 from location_field.models.plain import PlainLocationField
 
@@ -14,10 +18,40 @@ class Point(models.Model):
     location = PlainLocationField(based_fields=['city'], zoom=7, null=True)
     name = models.CharField(max_length=250)
     description = models.CharField(max_length=1000, blank=True, null=True)
-    group = models.ForeignKey(PointGroup, on_delete=models.CASCADE)
+    group = models.ForeignKey(PointGroup, on_delete=models.SET_NULL, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return str(self.id)+ " : "+ self.name    
+        return str(self.id)+ " : "+ self.name  
+
+class Period(models.Model):
+    name = models.CharField(max_length=250)
+    def __str__(self) -> str:
+        return self.name  
+
+class Event(models.Model):
+    name = models.CharField(max_length=250)
+    date = models.DateTimeField(null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+class AtrifactType(models.Model):
+    name = models.CharField(max_length=250)
+
     
+class Artifact(models.Model):
+    name = models.CharField(max_length=250)
+    description = models.CharField(max_length=1000, blank=True)
+    event = models.ForeignKey(Event, on_delete=models.SET_NULL, null=True)
+    period = models.ForeignKey(Period, on_delete=models.SET_NULL, null=True)
+    type = models.ForeignKey(AtrifactType, on_delete=models.SET_NULL, null=True)
+    year = models.CharField(max_length=20, null=True)
+    nominal = models.CharField(max_length=20, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+
+
+
